8ff9622aa221d081523a3d7a9e8536826b6466ea
Gregory Nevzorov <52013349+GregoryNevzorov@users.noreply.github.com>
2019-11-08 14:29:07 +0300

    Update main.cpp
    fix ru 3

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
 	char end = 'y';
 	while (end == 'y')
 	{
-		short int n, m; //Пользовательская размерность массива
+		short int n, m; //РџРѕР»СЊР·РѕРІР°С‚РµР»СЊСЃРєР°СЏ СЂР°Р·РјРµСЂРЅРѕСЃС‚СЊ РјР°СЃСЃРёРІР°
 		cout << "Only the matrix with the number of elements is less than 130 million.\n\n";
 		cout << "Enter the number of rows in the matrix: (use for input only arabic numeral 0-9)\n";
 		cout << "0 < input <= 32767\n";
@@ -17,11 +17,11 @@ int main()
 		cin >> m;
 
 		long int amount_of_elements = n * m;
-		if (amount_of_elements > 134217728) // Размер массива < 512 Мб.
+		if (amount_of_elements > 134217728) // Р Р°Р·РјРµСЂ РјР°СЃСЃРёРІР° < 512 РњР±.
 		{
 			cout << "Only the matrix with the number of elements is less than 130 million.\n";
 
-			//Проверка на повторный запуск программы.
+			//РџСЂРѕРІРµСЂРєР° РЅР° РїРѕРІС‚РѕСЂРЅС‹Р№ Р·Р°РїСѓСЃРє РїСЂРѕРіСЂР°РјРјС‹.
 			cout << "Run this program again now? (y/n) (one lowercase letter and 'Enter')\n";
 			cin >> end;
 		}
@@ -32,11 +32,10 @@ int main()
 
 			delete[] arr;
 
-			//Проверка на повторный запуск программы.
+			//РџСЂРѕРІРµСЂРєР° РЅР° РїРѕРІС‚РѕСЂРЅС‹Р№ Р·Р°РїСѓСЃРє РїСЂРѕРіСЂР°РјРјС‹.
 			cout << "Run this program again now? (y/n) (one lowercase letter and 'Enter')\n";
 			cin >> end;
 		}
 	}
 	return 0;
 }
-//
